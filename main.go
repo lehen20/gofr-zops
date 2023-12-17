7d719288d642567240ff609d634825bcf41fb4bd
lehen20 <zehralehen@gmail.com>
2023-12-17 21:53:56 +0530

    Final Changes

--- main.go
+++ main.go
@@ -2,22 +2,6 @@ package main
 
 import "gofr.dev/pkg/gofr"
 
-// func main() {
-//     // initialise gofr object
-//     app := gofr.New()
-
-//     app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-// 		// Get the value using the redis instance
-// 		value, err := ctx.Redis.Get(ctx.Context, "greeting").Result()
-
-//         return value, err
-//     })
-
-//     // Starts the server, it will listen on the default port 8000.
-//     // it can be over-ridden through configs
-//     app.Start()
-// }
-
 func main() {  
 	app := gofr.New()  
 	
@@ -28,8 +12,7 @@ func main() {
 	app.POST("/students", h.Create)  
 	app.PUT("/students/{id}", h.Update)  
 	app.DELETE("/students/{id}", h.Delete)  
-	
-	// starting the server on a custom port  
+	 
 	app.Server.HTTP.Port = 9092  
 	app.Start()  
   }
